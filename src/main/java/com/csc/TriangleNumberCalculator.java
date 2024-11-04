796ada9fa86ac2ae49289ac91ec2983357ddddd4
steveb2003 <nn80299@mail.harpercollege.edu>
2024-11-03 19:51:04 -0600

    triangle
    triangle

--- src/main/java/com/csc/TriangleNumberCalculator.java
+++ src/main/java/com/csc/TriangleNumberCalculator.java
@@ -1,5 +1,6 @@
 package com.csc;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TriangleNumberCalculator 
@@ -21,11 +22,23 @@ public class TriangleNumberCalculator
       
     int triangular_sum = add(triangle_number_n,triangle_number_m);
 
-    int triangular_difference= subtract(n=triangle_number_m,triangle_number_n);
+    int triangular_difference= subtract(triangle_number_m,triangle_number_n);
+
+    int triangular_product = multiply(triangle_number_n, triangle_number_m);
+
+    double triangular_quotient=divide(triangle_number_n, triangle_number_m);
+
+    String Triangle_List= sequence(n);
 
     System.out.printf("\nWhen n=%s and m=%s, the sum of the triangle numbers is %s + %s = %s", n, m, triangle_number_n, triangle_number_m, triangular_sum);
 
-    System.out.printf("\nWhen n=%s and m=%s, the difference of the triangle numbers is %s + %s = %s", n, m, triangle_number_n, triangle_number_m, triangular_difference);
+    System.out.printf("\nWhen n=%s and m=%s, the difference of the triangle numbers is %s - %s = %s", n, m, triangle_number_n, triangle_number_m, triangular_difference);
+
+    System.out.printf("\nWhen n=%s and m=%s, the product of the triangle numbers is %s * %s = %s", n, m, triangle_number_n, triangle_number_m, triangular_product);
+
+    System.out.printf("\nWhen n=%s and m=%s, the quotient of the triangle numbers is %s / %s = %s", n, m, triangle_number_n, triangle_number_m, triangular_quotient);
+
+    System.out.printf("\nThe sequence of triangle numbers leading to n=%s is: %s%n", n, Triangle_List);
 
     in.close();
   }
@@ -59,7 +72,36 @@ public class TriangleNumberCalculator
     int triangular_difference = triangle_number_m - triangle_number_n;
     return triangular_difference;
   }
+
+  public static int multiply(int triangle_number_n, int triangle_number_m)
+  {
+    int triangular_product = triangle_number_m * triangle_number_n;
+    return triangular_product;
+  }
+
+  public static double divide(int triangle_number_n, int triangle_number_m)
+  {
+    double n_divide = Double.valueOf(triangle_number_n);
+    double m_divide = Double.valueOf(triangle_number_m);
+    
+    double triangular_quotient = n_divide/m_divide;
+    return triangular_quotient;
+  }
+
+  public static String sequence(int n)
+  {
+    ArrayList<Integer> Triangle_Array_List = new ArrayList<>();
+    for (int i=1; i<=n;i++)
+    {
+      int Triangle_index = value(n-(n-i));
+      Triangle_Array_List.add(Triangle_index);
+    }
+    String Triangle_List= Triangle_Array_List.toString();
+    return Triangle_List;
+  }
+
 }
+
 
 
 
